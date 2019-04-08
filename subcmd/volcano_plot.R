9217146b096579153342622dd6142e55962122cb
YichaoLi <yl079811@ohio.edu>
2019-04-07 23:38:31 -0500

    volcano

--- subcmd/volcano_plot.R
+++ subcmd/volcano_plot.R
@@ -12,64 +12,31 @@ FDR_axis_name = args[8]
 Title = args[9]
 output_figure = args[10]
 
-# FC_cutoff = 1
-# FDR_cutoff = 0.01
-# locus = args[1]
-# output_file = args[2]
-
-res <- read.table(input_table, header=TRUE,sep=",")
+res <- read.table(input_table, header=TRUE,sep=seperator)
 
 keyvals <- rep('black', nrow(res))
 
 names(keyvals) <- rep('Not Significant', nrow(res))
 
-keyvals[which(res$gfp.logFC > FC_cutoff & res$gfp.padj < FDR_cutoff)] <- 'red'
-names(keyvals)[which(res$gfp.logFC > FC_cutoff & res$gfp.padj < FDR_cutoff)] <- 'Increased'
+keyvals[which(res[$LFC_column] > LFC_cutoff & res[$FDR_column] < FDR_cutoff)] <- 'red'
+names(keyvals)[which(res[$LFC_column] > LFC_cutoff & res[$FDR_column] < FDR_cutoff)] <- 'Increased'
 
 
-keyvals[which(res$gfp.logFC < -FC_cutoff & res$gfp.padj < FDR_cutoff)] <- 'blue'
-names(keyvals)[which(res$gfp.logFC < -FC_cutoff & res$gfp.padj < FDR_cutoff)] <- 'Decreased'
+keyvals[which(res[$LFC_column] < -LFC_cutoff & res[$FDR_column] < FDR_cutoff)] <- 'blue'
+names(keyvals)[which(res[$LFC_column] < -LFC_cutoff & res[$FDR_column] < FDR_cutoff)] <- 'Decreased'
 
 
  EnhancedVolcano(res,
     lab = rownames(res),
     selectLab =c(""),
-    x = 'gfp.logFC',
-    y = 'gfp.padj',
-    title = paste(locus,'gfp','Number of sgRNA:',dim(res)[1],sep=" "),
+    x = LFC_axis_name,
+    y = FDR_axis_name,
+    title = Title,
     colOverride = keyvals,
     colConnectors = 'grey50',
     pCutoff = FDR_cutoff,
-    FCcutoff = FC_cutoff,
+    FCcutoff = LFC_cutoff,
     transcriptPointSize = 1.5,
     transcriptLabSize = 3.0)
 
-ggsave(paste("gfp",output_file,sep=""),dpi=300)
-
-# keyvals <- rep('black', nrow(res))
-
-# names(keyvals) <- rep('Not Significant', nrow(res))
-
-# keyvals[which(res$apc.logFC > FC_cutoff & res$apc.padj < FDR_cutoff)] <- 'red'
-# names(keyvals)[which(res$apc.logFC > FC_cutoff & res$apc.padj < FDR_cutoff)] <- 'Increased'
-
-
-# keyvals[which(res$apc.logFC < -FC_cutoff & res$apc.padj < FDR_cutoff)] <- 'blue'
-# names(keyvals)[which(res$apc.logFC < -FC_cutoff & res$apc.padj < FDR_cutoff)] <- 'Decreased'
-
-
-#  EnhancedVolcano(res,
-#     lab = rownames(res),
-#     selectLab =c(""),
-#     x = 'apc.logFC',
-#     y = 'apc.padj',
-#     title = paste(locus,'apc','Number of sgRNA:',dim(res)[1],sep=" "),
-#     colOverride = keyvals,
-#     colConnectors = 'grey50',
-#     pCutoff = FDR_cutoff,
-#     FCcutoff = FC_cutoff,
-#     transcriptPointSize = 1.5,
-#     transcriptLabSize = 3.0)
-
-# ggsave(paste("apc",output_file,sep=""),dpi=300)
-
+ggsave(output_figure,dpi=300)
---
+++ subcmd/volcano_plot.py
@@ -0,0 +1,60 @@
+#!/hpcf/apps/python/install/2.7.13/bin/python
+from utils import *
+import os
+import argparse
+p_dir = os.path.dirname(os.path.realpath(__file__)) + "/"
+
+def arg_volcano_plot(parser):
+	cmd=parser.add_parser('volcano_plot',help='Data visualization: Volcano plot',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+	cmd.add_argument('-f','--input',  help="data table to be plot", required=True)
+	cmd.add_argument('-j',"--jid",  help="enter a job ID, which is used to make a new directory. Every output will be moved into this folder.", default='{{subcmd}}_'+username+"_"+str(datetime.date.today()))	
+
+	options=cmd.add_argument_group(title='options')
+	options.add_argument('-s','--seperator',  help="column seperator", default=',',type=str)
+	options.add_argument('--LFC_column',  help="column name for log fold change", default='LFC',type=str)
+	options.add_argument('--LFC_cutoff',  help="log fold change cutoff", default=2,type=float)
+	options.add_argument('--LFC_axis_name',  help="The axis name for log fold change", default='LFC',type=str)
+	options.add_argument('--FDR_column',  help="column name for p-value or FDR", default='FDR',type=str)
+	options.add_argument('--FDR_cutoff',  help="p-value or FDR cutoff", default=2,type=float)
+	options.add_argument('--FDR_axis_name',  help="The axis name for p-value or FDR", default='FDR',type=str)	
+	options.add_argument('--title',  help="Figure title", default='Volcano plot',type=str)
+	options.add_argument('--do_not_submit',  help="EnhancedVolcano is installed on rhel7 nodes, by default you are on rhel6 nodes, unless you did ``hpcf_interactive -q standard``, please don't use this option.", action='store_true')
+	options.add_argument('--output_figure',  help="output file name", default='Volcano_plot_'+username+"_"+str(datetime.date.today())+".png",type=str)
+
+
+def run_volcano_plot(args,rootLogger):
+	# rootLogger.info("this is volcano_plot")
+	# rootLogger.info(str(args.subcmd).lower())
+	if str(args.subcmd).lower() != "volcano_plot":
+		return 0		
+	if not os.path.isfile(args.input):
+		rootLogger.error("Input files do not exist!")
+		os.system("HemTools volcano_plot -h")
+		# rootLogger.close()
+		os.system("rm "+args.jid+".log")
+		sys.exit(1)
+	if os.path.isdir(args.jid):
+		addon_string = str(uuid.uuid4()).split("-")[-1]
+		rootLogger.info ("The input job id is not available!")
+		args.jid = args.jid+"_"+addon_string
+		rootLogger.info ("The new job id is: "+args.jid)
+	else:
+		rootLogger.info ("The job id is: "+args.jid)			
+		
+	commands = ['module load R/3.5.1']	
+	volcano_plot_command = ""p_dir+""
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
