b61c8d41c5ab0b609546478a15cf807cd219b2a0
Yichao Li <yl079811@ohio.edu>
2019-04-16 21:38:34 -0500

    Create disqus.js

---
+++ docs/_static/disqus.js
@@ -0,0 +1,8 @@
+(function() {  // DON'T EDIT BELOW THIS LINE
+	var d = document, s = d.createElement('script');
+	
+	s.src = 'https://docs-aviatrix-com.disqus.com/embed.js';
+	
+	s.setAttribute('data-timestamp', +new Date());
+	(d.head || d.body).appendChild(s);
+})();
