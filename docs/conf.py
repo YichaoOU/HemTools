3be4029b0e9c864ed5f3dfdd80b93c9be751e053
Yichao Li <yl079811@ohio.edu>
2019-04-16 21:20:42 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ language = None
 exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
