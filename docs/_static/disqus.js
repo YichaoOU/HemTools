66174629460213f49ae7075fbbe5882ac3ecfb42
Yichao Li <yl079811@ohio.edu>
2019-04-16 21:39:31 -0500

    Update disqus.js

--- docs/_static/disqus.js
+++ docs/_static/disqus.js
@@ -1,7 +1,7 @@
 (function() {  // DON'T EDIT BELOW THIS LINE
 	var d = document, s = d.createElement('script');
 	
-	s.src = 'https://docs-aviatrix-com.disqus.com/embed.js';
+	s.src = 'https://hemtools.disqus.com/embed.js';
 	
 	s.setAttribute('data-timestamp', +new Date());
 	(d.head || d.body).appendChild(s);
