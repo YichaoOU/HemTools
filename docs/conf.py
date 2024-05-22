7ce77eeaf32b534cc3de5ade6f7b96dc61a25906
Yichao <Yichao.Li@stjude.org>
2024-05-22 10:42:09 -0500

    update

--- docs/conf.py
+++ docs/conf.py
@@ -110,7 +110,7 @@ html_theme = 'sphinx_rtd_theme'
 
 
 # html_theme_path = ["_theme", ]
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = []
 
 html_theme_options = {
     'versions': [
